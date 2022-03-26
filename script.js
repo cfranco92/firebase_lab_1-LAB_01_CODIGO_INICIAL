4feba43105246b8f4676a6b7bdb2afe85d63a181
Cristian Franco <cfrancobedoya@gmail.com>
2022-03-26 10:28:53 -0500

    Add messages

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
+var usuarioAutenticado;
+
+$('#login').click(function () {
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+ 
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+
+            console.log(user)
+            guardarUsuario(user)
+
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+ });
 
 //Cuando preseiona click en botón "enviar"
 $('#enviar').click(function(){
     const texto = $('#btn-input').val();
     
-    adicionarMensaje(texto, true);
+    // adicionarMensaje(texto, true);  // Ya no la necesito porque reactivamente el chat se da cuenta
+    guardarMensajeEnFirebase(texto);
 
     $('#btn-input').val('');
     $('#btn-input').focus();
@@ -11,14 +43,14 @@ $('#enviar').click(function(){
 });
 
 //Funcion que adiciona lo que escribieron, al panel 
-function adicionarMensaje(texto, isMyMessage){
+function adicionarMensaje(texto, isMyMessage, avatar){
 
     const estiloBase = "base_sent";
     const estiloMsg  =  "msg_sent";
-    const avatar= "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg";
+    // const avatar= "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg";
 
     const divAvatar =   "    <div class='col-md-2 col-xs-2 avatar'>" +
-                        "        <img src='"+avatar+"' class=' img-responsive '>" +
+                        "        <img src='"+avatar+"' class=' img-responsive ' >" +
                         "    </div>";
 
     $('#msgContainer').append(
@@ -38,3 +70,54 @@ function adicionarMensaje(texto, isMyMessage){
     container.scrollTop = container.scrollHeight;
 
 }
+
+//Guardar un usuario en la BD de firebase
+function guardarUsuario(user){
+    usuarioAutenticado = {
+        nombre: user.displayName,
+        email: user.email,
+        foto: user.photoURL,
+        uid: user.uid
+    }
+
+    firebase.database().ref("usuarios/" + user.uid)
+        .set(usuarioAutenticado);
+
+}
+
+function guardarMensajeEnFirebase(textoMensaje){
+    const mensaje = {
+        fecha: Date.now(),
+        uid: usuarioAutenticado.uid,
+        text: textoMensaje
+    }
+
+    firebase.database().ref("mensajes")
+       .push(mensaje);
+}
+
+//De manera reactiva, quiero enterarme cuando un nuevo mensaje llega a la colección
+firebase.database().ref("mensajes")
+    .on("child_added", function(snapshotMensaje){
+        
+        //Si no se ha autenticado, no puede ver los mensajes
+        if (!usuarioAutenticado){
+            return;
+        }
+
+        //Un nuevo mensaje fue escrito en la colección "mensajes"
+        const mensaje = snapshotMensaje.val();
+        const uid = mensaje.uid;
+        
+        //Consulto el snapshot del usuario dueno del mensaje
+        firebase.database().ref("usuarios/" + uid)
+            .on("value", (snapshotUsuarioDuenoMensaje) => {
+                
+                const usuarioDuenoMensaje = snapshotUsuarioDuenoMensaje.val();
+                const esMiMensaje = mensaje.uid === usuarioAutenticado.uid;
+                
+                adicionarMensaje(mensaje.text, esMiMensaje, usuarioDuenoMensaje.foto);
+
+            });
+
+    });
